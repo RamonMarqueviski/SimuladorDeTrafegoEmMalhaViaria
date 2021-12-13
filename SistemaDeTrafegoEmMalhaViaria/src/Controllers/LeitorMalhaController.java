e824cf1eb40b189d8c4d44c1628f5d7803b758c9
jaozao337 <jsolagna@yahoo.com.br>
2021-12-13 15:38:35 -0300

    e
    nao tive tempo, tentei arrumar algo mas n consegui pensar

--- SistemaDeTrafegoEmMalhaViaria/src/Controllers/LeitorMalhaController.java
+++ SistemaDeTrafegoEmMalhaViaria/src/Controllers/LeitorMalhaController.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
 import Model.MalhaViaria;
 import View.Home;
 
-/**
- *
- * @author ramom
- */
 public class LeitorMalhaController {
 
     private Home telaInicial;
--- SistemaDeTrafegoEmMalhaViaria/src/Model/MalhaViaria.java
+++ SistemaDeTrafegoEmMalhaViaria/src/Model/MalhaViaria.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 import View.Home;
@@ -11,10 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTextArea;
 
-/**
- *
- * @author ramom
- */
+
 public class MalhaViaria extends Thread {
 
     ;
